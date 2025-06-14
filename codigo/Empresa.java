02dcbfa55ec0fa4d7699f8b2aee38cfd76d89c9c
Julia Ventura <68651976+juliaszventura@users.noreply.github.com>
2025-06-14 00:50:23 -0300

    Update Empresa

--- codigo/Empresa.java
+++ codigo/Empresa.java
@@ -1,2 +1,16 @@
-public class Empresa {
+public class Empresa extends Instituicao{
+    private int cnpj;
+
+    public void setNome(String Nome){
+        this.nome = nome;
+    }
+    public void getNome(String Nome){
+        return nome;
+    }
+    public int setCNPJ(int CNPJ){
+        this.cnpj = cnpj;
+    }
+    public int getCNPJ(){
+        return cnpj;
+    }
 }
