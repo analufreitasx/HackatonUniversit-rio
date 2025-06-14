8ec346b1cbf196b280c3d8980e3d07358368a497
Ana Luiza de Freitas <analuizafreitas12@yahoo.com.br>
2025-06-14 08:58:57 -0300

    Alteracoes Apresentacao

--- codigo/Banca.java
+++ codigo/Banca.java
@@ -1,4 +1,6 @@
+import java.util.Map;
+
 public class Banca implements Avaliavel {
     private Projeto projeto;
-    private Map<Jurado, Inetger> jurado;
+    private Map<Jurado, Integer> jurado;
 }
