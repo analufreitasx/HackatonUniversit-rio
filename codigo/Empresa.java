4ddbe1a158f2738517f2e5a2bf6bfb6c5cdac441
Julia Ventura <68651976+juliaszventura@users.noreply.github.com>
2025-06-14 11:14:50 -0300

    Construtores de Empresa e Universidade

--- codigo/Empresa.java
+++ codigo/Empresa.java
@@ -1,16 +1,23 @@
 public class Empresa extends Instituicao{
     private int cnpj;
 
+    public Empresa(String nome, int cnpj) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+
+    public void getNome(String nome){
+
+        return nome;
+    }
     public void setNome(String nome){
         this.nome = nome;
     }
-    public void getNome(String nome){
-        return nome;
+    public int getCNPJ(){
+
+        return cnpj;
     }
     public int setCNPJ(int CNPJ){
         this.cnpj = cnpj;
-    }
-    public int getCNPJ(){
-        return cnpj;
     }
 }
--- codigo/Universidade.java
+++ codigo/Universidade.java
@@ -1,16 +1,21 @@
 public class Universidade extends Instituicao{
     private int idUniversidade;
 
-    public void setNome(String nome){
+    public Universidade(String nome, int idUniversidade) {
         this.nome = nome;
+        this.idUniversidade = idUniversidade;
     }
+
     public String getNome(){
         return nome;
     }
-    public void setIdUniversidade(int idUniversidade){
-        this.idUniversidade = idUniversidade;
+    public void setNome(String nome){
+        this.nome = nome;
     }
     public int getIdUniversidade(){
         return idUniversidade;
+    }
+    public void setIdUniversidade(int idUniversidade){
+        this.idUniversidade = idUniversidade;
     }
 }
