eb78c0a42382cd2a175a494b2e3f6fa81b6d4797
Julia Ventura <68651976+juliaszventura@users.noreply.github.com>
2025-06-14 00:10:55 -0300

    get e set Projeto

--- codigo/Projeto.java
+++ codigo/Projeto.java
@@ -2,4 +2,24 @@ public class Projeto {
     private Profissional orientado;
     private Equipe equipe;
     private Int notaFinal;
+
+    //gets e sets
+    public void setProfissional(Profissional orientado) {
+        this.orientado = orientado;
+    }
+    public void setEquipe(Equipe equipe) {
+        this.equipe = equipe;
+    }
+    public void setNotaFinal(int notaFinal) {
+        this.notaFinal = notaFinal;
+    }
+    public Profissional getOrientado() {
+        return orientado;
+    }
+    public Equipe getEquipe() {
+        return equipe;
+    }
+    public int getNotaFinal() {
+        return notaFinal;
+    }
 }
