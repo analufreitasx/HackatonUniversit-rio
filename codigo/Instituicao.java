0b72a365bdba0f3663b39db4cb9096fa43cd08ca
Julia Ventura <68651976+juliaszventura@users.noreply.github.com>
2025-06-14 00:23:19 -0300

    Instituicao

--- codigo/Instituicao.java
+++ codigo/Instituicao.java
@@ -1,2 +1,8 @@
-public class Instituicao {
+public abstract class Instituicao {
+    private String nome;
+
+    //gets e sets
+
+    public abstract void setNome(String nome);
+    public abstract String getNome();
 }
--- codigo/Pessoa.java
+++ codigo/Pessoa.java
@@ -17,4 +17,3 @@ public class Pessoa {
         return instituicao;
     }
 }
-
