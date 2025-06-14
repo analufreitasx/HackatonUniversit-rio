b3babd6e4df6f6c409a2ed85d774f4c1f43dad24
Julia Ventura <68651976+juliaszventura@users.noreply.github.com>
2025-06-14 11:19:15 -0300

    Construtor de Sala

--- codigo/Sala.java
+++ codigo/Sala.java
@@ -1,10 +1,19 @@
 public class Sala {
     private int codigo;
 
+    public Sala(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
-    public int getCodigo() {
-        return codigo;
+
+    @Override
+    public String toString() {
+        return "Sala codigo: " + codigo;
     }
 }
