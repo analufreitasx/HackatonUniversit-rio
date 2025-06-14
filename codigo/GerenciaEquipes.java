1eb73d98831bfa534460af0068ca89aa61e27d13
Julia Ventura <68651976+juliaszventura@users.noreply.github.com>
2025-06-14 10:25:30 -0300

    BuscarEquipes

--- codigo/GerenciaEquipes.java
+++ codigo/GerenciaEquipes.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class gerenciaEquipes {
+public class GerenciaEquipes {
     private static GerenciaEquipes instancia;
     private List<Equipe> listaEquipes;
 
@@ -11,7 +11,7 @@ public class gerenciaEquipes {
 
     public static GerenciaEquipes getInstancia() {
         if (instancia == null) {
-            instancia = new Equipes();
+            instancia = new GerenciaEquipes();
         }
         return instancia;
     }
@@ -22,5 +22,14 @@ public class gerenciaEquipes {
 
     public List<Equipe> getTodasEquipes() {
         return listaEquipes;
+    }
+
+    public Equipe buscarPorNome(String nome) {
+        for (Equipe equipe : listaEquipes) {
+            if (equipe.getNome().equalsIgnoreCase(nome)) {
+                return equipe;
+            }
+        }
+        return null;
     }
 }
