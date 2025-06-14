4a3262bfccc65668f865b9890aaeba7ba140c916
Julia Ventura <68651976+juliaszventura@users.noreply.github.com>
2025-06-14 10:52:06 -0300

    Construtor classe Pessoa

--- codigo/Pessoa.java
+++ codigo/Pessoa.java
@@ -2,17 +2,23 @@ public abstract class Pessoa {
     private String nome;
     private Instituicao instituicao;
 
-    //gets e sets
-    public void setNome(String nome) {
+    //construtor
+    public Pessoa(Strig nome, Instituicao instituicao) {
         this.nome = nome;
-    }
-    public void setInstituicao(Instituicao instituicao) {
         this.instituicao = instituicao;
     }
+
+    //gets e sets
     public String getNome() {
         return nome;
     }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
     public Instituicao getInstituicao() {
         return instituicao;
+    }
+    public void setInstituicao(Instituicao instituicao) {
+        this.instituicao = instituicao;
     }
 }
